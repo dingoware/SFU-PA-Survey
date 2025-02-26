f6ce4180dcfd81944394c95b42f2857d14c8b370
Dingo <30963435+dingoware@users.noreply.github.com>
2025-02-26 13:37:35 -0500

    Fixed SwingUI not appearing; window is extremely small
    Signed-off-by: Dingo <30963435+dingoware@users.noreply.github.com>

--- src/display.java
+++ src/display.java
@@ -1,5 +1,15 @@
 package src;
 
-public class display {
+import javax.swing.*;
 
+public class display extends JFrame {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+
+    public static void main(String[] args) {
+        JFrame gui = new JFrame();
+        gui.show();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
+
