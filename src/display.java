a6bba22608bab74b8ab40fe8d5e889018ca376a0
Dingo <30963435+dingoware@users.noreply.github.com>
2025-03-01 21:46:42 -0500

    Fixed GUI not appearing correctly; can now switch through tabs and click on buttons.
    Signed-off-by: Dingo <30963435+dingoware@users.noreply.github.com>

--- src/display.java
+++ src/display.java
@@ -3,6 +3,7 @@ package src;
 import javax.swing.*;
 
 public class display extends JFrame {
+    //create GUI components
     private JTabbedPane tabbedPane1;
     private JPanel panel1;
     private JButton editButton;
@@ -11,11 +12,31 @@ public class display extends JFrame {
     private JList list1;
     private JRadioButton runGuestReportRadioButton;
     private JRadioButton runCourseReportRadioButton;
+    private JPanel dbPanel;
+    private JPanel csurvPanel;
+    private JPanel gsurvPanel;
+    private JPanel gradePanel;
+    private JPanel reportPanel;
+
+    public display() {
+        setTitle("PA Survey Database");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //add panels to tabbedPane
+        tabbedPane1.addTab("Database", dbPanel);
+        tabbedPane1.add("Course Surveys", csurvPanel);
+        tabbedPane1.add("Guest Surveys", gsurvPanel);
+        tabbedPane1.add("Grade Inputs", gradePanel);
+        tabbedPane1.add("Generate Report", reportPanel);
+
+        //add panel to display
+        add(tabbedPane1);
+    }
 
     public static void main(String[] args) {
-        JFrame gui = new JFrame();
+        display gui = new display();
         gui.setVisible(true);
-        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
 
