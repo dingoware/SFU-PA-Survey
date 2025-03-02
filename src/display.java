973dc67978e039226670db514a81113665cd264d
mangojelly <witchhoodie@gmail.com>
2025-03-01 19:52:46 -0500

    gui updated

--- src/display.java
+++ src/display.java
@@ -5,10 +5,16 @@ import javax.swing.*;
 public class display extends JFrame {
     private JTabbedPane tabbedPane1;
     private JPanel panel1;
+    private JButton editButton;
+    private JButton deleteButton;
+    private JButton saveButton;
+    private JList list1;
+    private JRadioButton runGuestReportRadioButton;
+    private JRadioButton runCourseReportRadioButton;
 
     public static void main(String[] args) {
         JFrame gui = new JFrame();
-        gui.show();
+        gui.setVisible(true);
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
